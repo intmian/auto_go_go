0719b82197d5273283e27b7274330ff1e20e77fd
mian <38103855+intmian@users.noreply.github.com>
2022-05-11 23:22:44 +0800

    修改打包脚本 + 更新配置 + linux适配

--- pack_linux.bat
+++ pack_linux.bat
@@ -5,6 +5,11 @@ if not exist pack\static md pack\static
 if not exist pack\http md pack\http
 go get -u github.com/intmian/mian_go_lib
 go mod tidy
+
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=amd64
+
 go build -o pack
 copy setting_tep.json pack\setting.json
 Xcopy http\static\ pack\http\static\ /Y /E
